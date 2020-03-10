37cc4acbbba76d06039ee19e4c66cd9300ce0a8a
oNickGoD <firenze.age@gmail.com>
2020-03-10 14:03:05 -0300

    incompleto aula 7

---
+++ Assets/scripts/NavGame/Core/TouchableGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+
+    public class TouchableGameObject : MonoBehaviour
+    {
+        public float contactRadius = 0.5f;
+        public bool IsInTouch(TouchableGameObject other)
+        {
+            float distance = Vector3.Distance(transform.position, other);
+            return distance < contactRadius + other.contactRadius;
+        } 
+
+        protected virtual void OnDrawGizmosSelected() 
+        {
+            Gismos.color = Color.yellow;
+            Gismos.DrawWireSphere(transform.position, contactRadius);
+        }
+    }
+}
